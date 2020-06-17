f4186366bbdb489683807162dda3181de9fc10a6
Utkarsh Tiwari <utiwari914@gmail.com>
2020-06-17 16:53:19 +0530

    reformat code using standard js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var port = process.env.PORT || 9000;
-require('dotenv').config();
-
-const cors = require('cors');
-const express = require('express');
+var port = process.env.PORT || 9000
+require('dotenv').config()
+const path = require('path')
+const cors = require('cors')
+const express = require('express')
 // const bodyparser = require('body-parser');
-const public = require('./routes/public');
-const api = require('./routes/api');
+const pub = require('./routes/public')
+const api = require('./routes/api')
 
-var app = express();
+var app = express()
 
 // set the view engine to ejs
 // app.set('view engine', 'ejs');
-app.use('/', express.static(__dirname+'/public'));
+app.use('/', express.static(path.join(__dirname, '/public')))
 
 // app.use(bodyparser.json());
 // app.use(bodyparser.urlencoded({extended: false}));
 
-app.use(cors());
+app.use(cors())
 
-app.use('/', public);
-app.use('/api', api);
+app.use('/', pub)
+app.use('/api', api)
 
-app.listen(port, ()=>{
-  console.log(`MUSIC-DL SERVER IS UP ON PORT ${port}`);
-});
+app.listen(port, () => {
+  console.log(`MUSIC-DL SERVER IS UP ON PORT ${port}`)
+})
--- routes/api.js
+++ routes/api.js
@@ -1,45 +1,42 @@
 const express = require('express')
 const ytdl = require('ytdl-core')
-var search = require('youtube-search');
+var search = require('youtube-search')
 // const https = require('https')
 const router = express.Router()
 
 var opts = {
   maxResults: 1,
   key: process.env.KEY
-};
+}
 
-router.get('/search', (req, res)=>{
-	let q = req.query.q
-	search(q, opts, (err, list)=>{
-		if (err || !list[0]) {
-			res.status(201).send({ error: true, message: 'Error Occured! Try again.' })
-			throw err;
-		}
-		else {
-			console.log(list[0])
-			res.status(200).json({ error: false, video: list[0] })
-		}
-	})
+router.get('/search', (req, res) => {
+  const q = req.query.q
+  search(q, opts, (err, list) => {
+    if (err || !list[0]) {
+      res.status(201).send({ error: true, message: 'Error Occured! Try again.' })
+      throw err
+    } else {
+      console.log(list[0])
+      res.status(200).json({ error: false, video: list[0] })
+    }
+  })
 })
 
-router.get('/download', async (req, res)=>{
-	try {
-		let { id, name } = req.query;
-		let URL = `https://youtube.com/watch?v=${id}`
-	  // res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-	  res.header('Content-Type', 'application/octet-stream')
-	  	.header(`content-disposition`, `attachment; filename=${name}.mp3;`);
+router.get('/download', async (req, res) => {
+  try {
+    const { id, name } = req.query
+    const URL = `https://youtube.com/watch?v=${id}`
+    // res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    res.header('Content-Type', 'application/octet-stream')
+      .header('content-disposition', `attachment; filename=${name}.mp3;`)
 
-		ytdl(URL, {
-    	format: 'mp3'
+    ytdl(URL, {
+      format: 'mp3'
     })
-    .pipe(res);
-	}
-	catch(e) {
-		res.status(400).send({ error: true, message: 'Error Occured!' })
-	}
+      .pipe(res)
+  } catch (e) {
+    res.status(400).send({ error: true, message: 'Error Occured!' })
+  }
 })
 
-
-module.exports = router;
+module.exports = router
